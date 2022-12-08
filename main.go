77c0f1eed824c69e5180c8593878d0c20c4c7692
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-12-08 18:02:09 +0700

    add view user image route

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", controllers.HandleForm)
 	http.HandleFunc("/submit", controllers.HandleSubmit)
+	http.HandleFunc("/users/image", controllers.HandleShowImage)
 
 	fmt.Println("server running on port 3000")
 	http.ListenAndServe(":3000", nil)
